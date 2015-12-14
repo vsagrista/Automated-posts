7f9a8610b980340d41f6a97fe8b2eca4bc0002f6
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-12-14 16:08:19 +0100

    Went back to previous config with env keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'TWITTER_KEY', 'TWITTER_SECRET'
-  #provider :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"]#, scope: 'email, publish_actions'
+  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+ # provider :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"]#, scope: 'email, publish_actions'
 end
 
 OmniAuth.config.on_failure = Proc.new do |env|
